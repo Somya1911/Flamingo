e15dbb301feeeb5322e1a0c9ae31e03cf59c7116
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2021-03-15 00:29:21 +0530

    done sar

--- lib/main.dart
+++ lib/main.dart
@@ -20,93 +20,90 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      //backgroundColor:Colors.white ,
       body: Stack(
         children: <Widget>[
-            Container(
-              height: size.height,
-              width: size.width,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                image: AssetImage('assets/images/flamingo_background.png'),
-                fit: BoxFit.cover
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            color: Color(0xffE7C6DE),
+          ),
+          Container(
+            child: Image.asset(
+              'assets/images/flamingo_background.png',
+              fit: BoxFit.cover,
+            ),
+            // height: size.height,
+            // width: size.width,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              // Container(
+              //   margin: EdgeInsets.only(top: size.height * 1 / 100),
+              //   child: Center(
+              //     child: Text(
+              //       "Login",
+              //       style: TextStyle(
+              //           color: Colors.black, fontSize: 40, fontWeight: FontWeight.bold),
+              //     ),
+              //   ),
+              // ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: size.width * 5 / 100),
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0xffBC4889),
+                      blurRadius: 20.0,
+                      offset: Offset(0, 10),
+                    ),
+                  ],
                 ),
-              ),
-              child: (
-                 Container(
-                        margin: EdgeInsets.only(top: 50),
-                          child: Center(
-                          child: Text("Login", style: TextStyle(color: Colors.black, fontSize: 40, fontWeight: FontWeight.bold))
-                        )
-                      ))
-
-              ),
-              Padding(
-                padding: EdgeInsets.all(30.0),
                 child: Column(
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color.fromRGBO(143,148,251,2),
-                            blurRadius: 20.0,
-                            offset: Offset(0,10)
-
-
-                          )
-                        ]
-                      ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
                     Container(
                       padding: EdgeInsets.all(8.0),
                       decoration: BoxDecoration(
-                        border: Border(bottom: BorderSide(color: Colors.grey[100]))
+                        border: Border(
+                          bottom: BorderSide(color: Colors.grey[100]),
+                        ),
                       ),
                       child: TextField(
                         decoration: InputDecoration(
                           border: InputBorder.none,
                           hintText: "Email or Phone number",
-                          hintStyle: TextStyle(color: Colors.grey[400])
-                        )
-                      )
+                          hintStyle: TextStyle(color: Colors.grey[400]),
+                        ),
+                      ),
                     ),
                     Container(
-                        padding: EdgeInsets.all(8.0),
-                        decoration: BoxDecoration(
-                            border: Border(bottom: BorderSide(color: Colors.grey[100]))
+                      padding: EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(color: Colors.grey[100]),
                         ),
-                        child: TextField(
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: "Password",
-                                hintStyle: TextStyle(color: Colors.grey[400])
-                            )
-                        )
-                    )
-                  ]
-                      )
-                    )
-                  ]
-                )
-              )
-    ],
-    )
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Password",
+                          hintStyle: TextStyle(color: Colors.grey[400]),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: size.height * 10 / 100,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
-
   }
-
 }
-
-
-
-
-
-
-
-
